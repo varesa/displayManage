96cea1c829aae1ca66db67d3ccb4ef4374c5ed7e
Esa Varemo <esa@kuivanto.fi>
2013-11-25 09:25:46 -0500

    Pyramid default project

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'pyramid_debugtoolbar',
+    'waitress',
+    ]
+
+setup(name='app',
+      version='0.0',
+      description='app',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="app",
+      entry_points="""\
+      [paste.app_factory]
+      main = app:main
+      """,
+      )
