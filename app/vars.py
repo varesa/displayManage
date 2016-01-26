6e876554805841f28ae90c1826d5fe58e95edfea
Esa Varemo <esa@kuivanto.fi>
2016-01-27 00:50:04 +0200

    create directory structure on (first) start

--- app/vars.py
+++ app/vars.py
@@ -1,15 +1,23 @@
 import os
+import sys
 
 # Paths
 
 basepath = "/displaymanage/"
+if not os.path.isdir(basepath):
+    print("Not installed in " + basepath)
+    sys.exit(1)
 
 datapath = os.path.join(basepath, "data")
+if not os.path.isdir(datapath):
+    os.mkdir(datapath)
 
 matrixpath = os.path.join(datapath, "matrix.json")
 devicespath = os.path.join(datapath, "devices.json")
 
 logpath = os.path.join(basepath, "logs/")
+if not os.path.isdir(logpath):
+    os.mkdir(logpath)
 
 # S3
 
