01077f74549892a88334f5c72dba6c57fafccf60
Esa Varemo <esa@kuivanto.fi>
2016-03-02 16:21:58 +0200

    Add connection manager to puppet installation

--- app/static/install.sh
+++ app/static/install.sh
@@ -4,8 +4,10 @@ apt-get update && apt-get install -y git puppet
 
 git clone https://github.com/varesa/puppet_nastori_system.git /etc/puppet/modules/nastori_system/
 git clone https://github.com/varesa/puppet_nastori_viewer.git /etc/puppet/modules/nastori_viewer/
+git clone https://github.com/varesa/puppet_nastori_connection.git /etc/puppet/modules/nastori_connection/
 
 puppet module install puppetlabs-inifile
+puppet module install puppetlabs-vcsrepo
 
 puppet apply -e "include nastori_system"
 puppet apply -e "include nastori_viewer"
